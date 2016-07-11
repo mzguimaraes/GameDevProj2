dcd4c83a30f7e6dbd8a7066a827d8240368f08fa
Marcus Guimaraes <mguimaraes42@gmail.com>
2016-07-11 15:53:04 -0400

    added TapGame

---
+++ Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using UnityEngine.UI; //contains Text and other UI-related classes
+
+public class HelloWorld : MonoBehaviour {
+
+	public Text textObject;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("Hello World!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		textObject.text = (Time.deltaTime / ((float) 1/60)).ToString();
+	}
+}
---
+++ Assets/Scripts/TapGame.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using UnityEngine.UI;
+
+public class TapGame : MonoBehaviour {
+
+	public Text textObject;
+
+	private int tapCount = 0;
+	private bool hasWon = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if ( Input.GetKeyDown(KeyCode.Space) && !hasWon) {
+			tapCount++;
+			textObject.text = "CURRENT SCORE: " + tapCount.ToString();
+		}
+		if (Input.GetKeyDown(KeyCode.C) && !hasWon) {
+			tapCount+= 100;
+			textObject.text = "CURRENT SCORE: " + tapCount.ToString();
+		}
+		if (tapCount > 101) {
+			hasWon = true;
+			textObject.text = "YOU WIN!!!!";
+		}
+
+	}
+}
