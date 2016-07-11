1d8df785a9a6fd39c3d9f53769d1b941240e4ab3
Marcus Guimaraes <mguimaraes42@gmail.com>
2016-07-11 16:33:54 -0400

    begin textAdventure

---
+++ Assets/Scripts/worldEngine.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using UnityEngine.UI;
+
+public class worldEngine : MonoBehaviour {
+
+	private enum rooms {
+		Lobby, 
+		Elevators, 
+		Outside
+	}
+	private string currentRoom = rooms.Lobby;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		string textBuffer = "You are currently in: " + currentRoom;
+
+		if (currentRoom == "Lobby") {
+			textBuffer += "\nYou see the security guard.";
+
+			textBuffer += "\npress [W] to go to elevators";
+			textBuffer += "\npress [S] to go outside";
+
+			if (Input.GetKeyDown(KeyCode.W))
+				currentRoom = rooms.Elevators;
+			else if (Input.GetKeyDown(KeyCode.S))
+				currentRoom = rooms.Outside;
+		}
+		else if (currentRoom == "Elevators") {
+			//elevators code
+		}
+		else if (currentRoom == "Outside") {
+			//outside code
+		}
+
+		GetComponent<Text>().text = textBuffer;
+	}
+}
